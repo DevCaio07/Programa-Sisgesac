7bcdd46e91905679f70bcadc91183c0adb36a6a5
Caio Guimaraes <caiotech.devbr@gmail.com>
2026-09-22 22:10:56 -0300

    first commit

---
+++ src/model/PontoPresenca.py
@@ -0,0 +1,7 @@
+class PontoPresenca:
+    def __init__(self,idGesac, uploaad,downnload,beam):
+        self.idGesac = idGesac 
+        self.uploaad = uploaad
+        self.downnload= downnload
+
+        
