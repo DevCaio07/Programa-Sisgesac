7ca22137d20264198f04404605db5e7fc7cbbd59
Caio Guimaraes <caiotech.devbr@gmail.com>
2026-09-22 23:01:33 -0300

    second commmit

--- src/model/PontoPresenca.py
+++ src/model/PontoPresenca.py
@@ -1,7 +1,25 @@
-class PontoPresenca:
-    def __init__(self,idGesac, uploaad,downnload,beam):
-        self.idGesac = idGesac 
-        self.uploaad = uploaad
-        self.downnload= downnload
+from abc import ABC, abstractmethod
 
-        
+class ponto_presenca: 
+    def __init__(self, idGesac, upload, download, beam):
+        self.idGesac = idGesac
+        self.upload = upload
+        self.download = download
+        self.beam = beam
+
+
+    def consultar(self): 
+        return{
+
+            "idGesac": self.idGesac,
+            "Upload:": self.upload, 
+            "Download": self.download,
+            "Beam": self.beam
+        }    
+
+    @abstractmethod
+    def cadastrar(self, bd):
+        pass
+
+    #O que aconteceu aqui? 
+    #``
